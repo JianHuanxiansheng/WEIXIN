03900c35b45cae60baf24ef056e44f556461439a
JianHuanxiansheng <65215874+JianHuanxiansheng@users.noreply.github.com>
2022-09-15 19:21:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def get_weather(region2):
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     key = config["weather_key"]
-    region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region2, key)
+    region2_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region2, key)
     response = get(region2_url, headers=headers).json()
     if response["code"] == "404":
         print("推送消息失败，请检查地区名是否有误！")
